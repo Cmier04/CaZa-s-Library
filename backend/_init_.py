29baef0529f144317c8b068306de74531a8d3368
Cmier04 <christine.n.mier@gmail.com>
2025-07-19 16:40:01 -0700

    first file structure update

---
+++ backend/_init_.py
@@ -0,0 +1,4 @@
+# Names: Alexia Macias, Christine Mier, Zhi Xuan Chong, Allen Le
+# Start Date: July 18, 2025
+# Project Name: CaZa's Library
+# File: __init__.py
---
+++ backend/functions.py
@@ -0,0 +1,15 @@
+# Define functions needed for program
+# comments to explain what each function does (one to three words)
+
+import json
+import os
+
+def load_users():
+    'load users from users.json'
+    #TODO: open the file and check if the file exists, return info
+
+def save_users(users):
+    'saves the users to users.json'
+    #TODO: implement the function by opening the file and writing in it
+
+def authenticate()
